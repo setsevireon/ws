a75058a8090886fbd551ad1b37b109fe3d2f5370
Carlos Marx <me@carlosmarx.com>
2024-01-02 14:33:47 -0300

    chore: savepoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-fn main() -> anyhow::Result<()> {
+use anyhow::Result;
+
+fn main() -> Result<()> {
     let body = reqwest::blocking::get("https://lpw6001tws03.unix.fidelidademundial.com:31116/twsd")?.text()?;
 
     println!("body = {:?}", body);
