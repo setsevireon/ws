f9b885da1bf407d45b116181f9644ff059528faf
Carlos Marx <me@carlosmarx.com>
2024-01-02 14:19:29 -0300

    chore: savepoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-fn main() {
-    let body = reqwest::get("https://lpw6001tws03.unix.fidelidademundial.com:31116/twsd")?.text()?;
+async fn main() {
+    let body = reqwest::get("https://lpw6001tws03.unix.fidelidademundial.com:31116/twsd")
+        .await?
+        .text()
+        .await?;
 
     println!("body = {:?}", body);
 }
