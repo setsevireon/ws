9a833859e8fcedcbc239255df446725e7839c96a
Carlos Marx <me@carlosmarx.com>
2024-01-02 14:48:50 -0300

    chore: savepoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
+use reqwest::blocking::ClientBuilder;
 
 fn main() -> Result<()> {
-    let body = reqwest::blocking::get("https://lpw6001tws03.unix.fidelidademundial.com:31116/twsd")?.text()?
+    let client = ClientBuilder::new().unwrap()..danger_accept_invalid_certs(true).build().unwrap();
+    let body = client.get("https://lpw6001tws03.unix.fidelidademundial.com:31116/twsd")?.text()?;
 
     println!("body = {:?}", body);
 
